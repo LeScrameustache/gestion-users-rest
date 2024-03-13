c25cee89f57aa0549c9e2f134a145384b72b7c2f
Le Scrameustache <thomas.temprano@mailo.com>
2024-03-13 15:36:04 +0100

    serveur lancé

--- src/main/java/eu/fr/mulesoft/gestion/controller/UserController.java
+++ src/main/java/eu/fr/mulesoft/gestion/controller/UserController.java
@@ -1,12 +1,15 @@
 package eu.fr.mulesoft.gestion.controller;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/users")
 public class UserController {
 	
 	@GET
-	
+	@Produces(MediaType.APPLICATION_JSON)
 	public String hello() {
 		return "Hello world !";
 	}
--- src/main/java/eu/fr/mulesoft/gestion/launch/Main.java
+++ src/main/java/eu/fr/mulesoft/gestion/launch/Main.java
@@ -1,9 +1,10 @@
 package eu.fr.mulesoft.gestion.launch;
 
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("api")
-public class Main {
+public class Main extends Application {
 
 	public static void main(String[] args) {
 		
